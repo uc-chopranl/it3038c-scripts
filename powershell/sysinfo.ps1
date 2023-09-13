5bd784d388bd6f754048915ea62cb4354eb53575
uc-chopranl <chopranl@mail.uc.edu>
2023-09-13 19:51:42 -0400

    powershelll files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,17 @@
+﻿$Hello = "Hello, PowerShell"
+Write-Host($Hello)
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+function getIP{
+
+    (get-netipaddress).ipv4address | Select-String "192*"
+
+}
+
+write-host(getIP)
+
+$IP = getIP
+
+Write-Host("This machine's IP is $IP")
+
+Write-Host("This machine's IP is {0}" -f $IP)
